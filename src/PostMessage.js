254e00185d5238b0ec58c4938befa424e50cce38
ozaki25 <fs65ugumori@yahoo.co.jp>
2019-09-17 10:14:17 +0900

    add error toast

--- src/PostMessage.js
+++ src/PostMessage.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react';
-import { Box, Button, Input } from '@chakra-ui/core';
+import { Box, Button, Input, useToast } from '@chakra-ui/core';
 
 const url = process.env.REACT_APP_API_URL;
 
 function PostMessage() {
+  const errorToast = useToast();
   const [content, setContent] = useState('');
 
   const onChange = e => {
@@ -12,12 +13,24 @@ function PostMessage() {
   };
 
   const onClick = async () => {
+    if (!content) {
+      showErrorToast();
+      return;
+    }
     fetch(`${url}/messages`, {
       method: 'post',
       headers: { 'content-type': 'application/json' },
       body: JSON.stringify({ content }),
     });
   };
+
+  const showErrorToast = () => {
+    errorToast({
+      description: 'コメントを入力してください',
+      status: 'error',
+    });
+  };
+
   return (
     <Box p={4}>
       <Input mb={4} defaultValue={content} onChange={onChange} />
